dc2fcd799a1d9e79ec02cf6a51efa7084faeb18a
xtexChooser <xtexchooser@duck.com>
2023-01-30 07:53:51 +0800

    ChiTangUI_unpriv: fix ERR_FILE_NOT_FOUND

--- src/chitangui/chitangui_unpriv/fun/FunActivity.java
+++ src/chitangui/chitangui_unpriv/fun/FunActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PowerManager;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.JavascriptInterface;
@@ -21,7 +22,7 @@ public class FunActivity extends Activity {
 
         {
             WebView webView = new WebView(this);
-            webView.loadUrl("file:///android_assets/fun.html");
+            webView.loadUrl("file:android_asset/fun.html");
             webView.setWebViewClient(new WebViewClient(){
                 @Override
                 public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -33,6 +34,16 @@ public class FunActivity extends Activity {
             webView.addJavascriptInterface(new JavaScriptInterface(), "chitangui_fun");
 
             layout.addView(webView);
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finishActivity(2);
+            return true;
+        } else {
+            return super.onKeyDown(keyCode, event);
         }
     }
 
